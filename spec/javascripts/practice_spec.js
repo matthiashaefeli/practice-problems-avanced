7d9be3a36e240c097723eb77cf7189667d0d6dbb
matthiashaefeli <haefeli.matthias@gmail.com>
2017-11-09 17:11:31 -0600

    last change on solution

--- spec/javascripts/practice_spec.js
+++ spec/javascripts/practice_spec.js
@@ -204,40 +204,6 @@ describe('updateInventory', function() {
 	});
 });
 
-describe('permAlone', function() {
-	it('Return the number of total permutations of the provided string that dont have repeated consecutive letters', function() {
-		expect(permAlone('aab')).toEqual(2);
-	});
-
-	it('Return the number of total permutations of the provided string that dont have repeated consecutive letters', function() {
-		expect(permAlone('aaa')).toEqual(0);
-	});
-
-	it('Return the number of total permutations of the provided string that dont have repeated consecutive letters', function() {
-		expect(permAlone('aabb')).toEqual(8);
-	});
-
-	it('Return the number of total permutations of the provided string that dont have repeated consecutive letters', function() {
-		expect(permAlone('abcdefa')).toEqual(3600);
-	});
-
-	it('Return the number of total permutations of the provided string that dont have repeated consecutive letters', function() {
-		expect(permAlone('abfdefa')).toEqual(2640);
-	});
-
-	it('Return the number of total permutations of the provided string that dont have repeated consecutive letters', function() {
-		expect(permAlone('zzzzzzzz')).toEqual(0);
-	});
-
-	it('Return the number of total permutations of the provided string that dont have repeated consecutive letters', function() {
-		expect(permAlone('a')).toEqual(1);
-	});
-
-	it('Return the number of total permutations of the provided string that dont have repeated consecutive letters', function() {
-		expect(permAlone('aaabb')).toEqual(12);
-	});
-});
-
 describe('Person', function() {
 	it('returns first name of Person after create a new Person', function() {
 		var bob = new Person('Bob Ross');
